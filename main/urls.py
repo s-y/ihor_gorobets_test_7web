5a87e0615e14a93e2f6d271c178e3414aa217ecb
Singularity <singularitty@gmail.com>
2014-01-16 16:02:38 +0200

    t6 Change form to Use Ajax to create new text note.

---
+++ main/urls.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function, unicode_literals
+
+from django.conf.urls import include, patterns, url
+
+from .views import *
+
+urlpatterns = patterns('',
+                       # Examples:
+                       # url(r'^$', 'webpages.views.home', name='home'),
+                       url(r'^note/add$', CreateNoteView.as_view(),
+                           name='note_add'),
+                       url(r'^$', ListNoteView.as_view(), name='notes'),
+                       url(r'^widget/', widget_view, name='widget'),
+                       # url(r'^blog/', include('blog.urls')),
+
+                       )
--- main/views.py
+++ main/views.py
@@ -19,9 +19,41 @@ from .models import *
 
 
 
-class CreateNoteView(CreateView):
+class AjaxableResponseMixin(object):
+
+    def render_to_json_response(self, context, **response_kwargs):
+        data = json.dumps(context)
+        response_kwargs['content_type'] = 'application/json'
+        return HttpResponse(data, **response_kwargs)
+
+    def form_invalid(self, form):
+        response = super(AjaxableResponseMixin, self).form_invalid(form)
+        if self.request.is_ajax():
+            data = form.errors
+            data['_error'] = True
+            return self.render_to_json_response(data)
+        else:
+            return response
+
+    def form_valid(self, form):
+        response = super(AjaxableResponseMixin, self).form_valid(form)
+        if self.request.is_ajax():
+            data = dict(form.data)
+            del data['csrfmiddlewaretoken']
+            data['_error'] = False
+            return self.render_to_json_response(data)
+        else:
+            return HttpResponse("Request is not ajax")
+
+
+class ListNoteView(ListView):
+    model = Note
+    paginate_by = 10
+    template_name = 'base/notes.html'
+
+
+class CreateNoteView(AjaxableResponseMixin, CreateView):
     model = Note
     form_class = AddNoteForm
     template_name = 'base/form.html'
     success_url = '/'
-
